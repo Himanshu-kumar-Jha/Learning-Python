2d99f6b45f51be8f92e386717ff94b1cb959e5d5
Himanshu <himanshu.sde@gmail.com>
2025-03-06 01:16:38 +0530

    More with lists

--- 02_Mutable_and_immutable.py
+++ 02_Mutable_and_immutable.py
@@ -1,3 +1,23 @@
+'''
+Immutable: The value of the object can't be changed once it is created.
+Mutable: The value of the object can be changed once it is created.
+if you copy a mutable object, and change the value of the copied object, the original object will also change.
+to avoid this use copy.deepcopy() method
+
+some mutable objects are:
+1. List
+2. Dictionary
+3. Set
+4. User-defined classes
+
+some immutable objects are:
+1. int
+2. float    
+3. decimal
+4. complex
+5. bool
+6. string
+'''
 x=10
 y=x
 x=5
--- 03_Exporing_list.py
+++ 03_Exporing_list.py
@@ -4,6 +4,11 @@ import copy
 #They can also contain duplicates
 My_list=[1,2,3,'Himanshu',True]
 
+#Declaring an empty list of size n 
+n=5
+empty_list=[None]*n
+print(empty_list)
+
 ####Methods in Lists
 
 #1.append ->to insert ele at end of the list
@@ -39,8 +44,12 @@ del New_deep[1]
 
 #5. sort a list 
 Numbers=[1,2,3,4,5,6]
+strings = ["a","bb","ccc","dddd"]
 Numbers.sort()  #for inc order
 Numbers.sort(reverse=True) #for dec order
+Numbers.sort(key=abs) #for sorting based on absolute values
+Numbers.sort(key=lambda x:x%2) #for sorting based on even and odd
+strings.sort(key=len) #for sorting based on length
 print(Numbers)
 
 #Slicing with lists
@@ -53,8 +62,33 @@ print(Slice[2:])
 Slice[1:3]=["Green","Tea"]
 print(Slice)
 
-#For loop
+#For loopṇ
 for i in Slice:
     print(i,end='->')
     if(i==4) :print("LOOL")
+#reverse a list
+Slice.reverse()
+print(Slice)
+#find the index of a particular element
+print(Slice.index(4))
+#count the number of occurences of a particular element
+print(Slice.count(4))
+
+#extend a list
+
+# Original list
+my_list = [1, 2, 3]
+
+# List to extend with
+extension = [4, 5, 6]
+
+# Extend the original list
+my_list.extend(extension)
+
+# Output the extended list
+print(my_list)  # Output: [1, 2, 3, 4, 5, 6]
+
+#remove an element
+my_list.remove(4)
+print(my_list)
 
